f89b8f266db445f6ac186646eff5c626d85df02a
Fer Enoch <ferenochsoames@gmail.com>
2023-03-19 13:37:50 -0300

    Header, AboutUs & Menu completed styles

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+// images
+import { images } from '../../constants';
+
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: '1rem' }}>
+    <p className="p__cormorant">
+      {title}
+    </p>
+    <img
+      src={images.spoon}
+      alt="Spoon" />
   </div>
 );
 
