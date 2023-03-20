66177de5f65f765508797e3c76685556293ab691
Fer Enoch <ferenochsoames@gmail.com>
2023-03-20 19:54:52 -0300

    web completed

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
-
+import React, { useRef } from 'react';
+// components
+import { SubHeading } from '../../components';
+// assets
+import { images } from '../../constants';
+// icons
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+// css
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  const scrollRef = useRef();
+  const galleryImages = [
+    images.gallery01,
+    images.gallery02,
+    images.gallery03,
+    images.gallery04,
+  ]
+
+  const scroll = direction => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  }
+
+  return (
+    <div className='app__gallery flex__center'>
+      <div className="app__gallery-content">
+        <SubHeading title='Instagram' />
+        <h1 className="headtext__cormorant">Photo gallery</h1>
+        <p className="p__opensans" style={{ color: '#AAA' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type='button' className='custom__button'>View More</button>
+      </div>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {
+            galleryImages.map((image, index) => {
+              return (
+                <div className="app__gallery-images_card flex__center" key={`gallery_image-${index}`}>
+                  <img src={image} alt="gallery_image" />
+                  <BsInstagram className='gallery__image-icon' />
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')} />
+          <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')} />
+        </div>
+      </div>
+    </div >
+  );
+}
 
 export default Gallery;
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
-
+// components
+import { SubHeading } from '../../components';
+// assets
+import { images, data } from '../../constants';
+// css
 import './Laurels.css';
 
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => {
+  return (
+    <div className="app__laurels_awards-card">
+      <img src={imgUrl} alt="award" />
+      <div className="app__laurels_awards-card_content">
+        <p className="p__cormorant" style={{ color: '#DCCA97' }}>
+          {title}
+        </p>
+        <p className="p__cormorant">
+          {subtitle}
+        </p>
+      </div>
+    </div>
+  );
+}
+
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id='awards'>
+    <div className="app__wrapper_info">
+      <SubHeading title='Awards & recognition' />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+      <div className="app__laurels_awards">
+        {
+          data.awards.map((award) => {
+            return (
+              <AwardCard award={award} key={award.title} />
+            )
+          })
+        }
+      </div>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels" />
+    </div>
   </div>
 );
 
